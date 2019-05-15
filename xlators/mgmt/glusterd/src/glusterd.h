1c582f5695a7800a8115fad9a01edfd3c844f746
baul <roidinev@gmail.com>
2019-05-16 06:32:41 +0800

    Revert "xlator: do not call dlclose() when debugging"
    This reverts commit 67dd12ff86181764bb6490df5dd4e743f3cf0101.

--- xlators/mgmt/glusterd/src/glusterd.h
+++ xlators/mgmt/glusterd/src/glusterd.h
@@ -173,6 +173,7 @@ typedef struct {
                                                  * transaction ids */
 
         struct cds_list_head       mount_specs;
+        gf_boolean_t               valgrind;
         pthread_t                  brick_thread;
         void                      *hooks_priv;
 
